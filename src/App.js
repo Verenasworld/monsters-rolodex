c2872d49bbe7d6f930987e41608e41ec68bc5ab9
Verenasworld <verena.felizidas@gmail.com>
2021-01-04 11:21:47 +0100

    adding files for gihub pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 import './App.css';
 
 
@@ -11,6 +12,7 @@ class App extends Component {
             monsters: [],
             searchField: ''
         };
+
     }
 
     componentDidMount() {
@@ -19,13 +21,23 @@ class App extends Component {
             .then(users => this.setState({monsters: users}));
     }
 
+    handleChange = (e) => {
+        this.setState({searchField: e.target.value});
+    }
+
     render() {
-        const { monsters, searchField} = this.state;
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+        );
         return (
             <div className="App">
-                <input type= 'search' placeholder= ' search monsters' onChange={e => this.setState({searchField: e.target.value}) }/>
-              <CardList monsters={this.state.monsters}>
-              </CardList>
+                <h1> Monsti Roloshizzel</h1>
+                <SearchBox
+                    placeholder='search Monster'
+                    handleChange={this.handleChange}
+                />
+                <CardList monsters={filteredMonsters}>
+                </CardList>
             </div>
         );
     }
